1e82f63a3f4901d35631790752429d5c721941b9
Jabbar Shaikh <87539750+Js112001@users.noreply.github.com>
2023-06-21 17:51:44 +0530

    employee_updated.dart

--- employee.dart
+++ employee.dart
@@ -1,29 +1,20 @@
 import 'dart:math';
 
 class Employee {
-  int? _id;
-  String? _name;
-  int? _age;
-  String? _city;
-  int? _salary;
+  String? name;
+  int? age;
+  String? city;
+  int? salary;
 
-  set id(int id) => this._id = id;
-  int get id => this._id!;
+  Employee(String name, int age, String city, int salary) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+    this.salary = salary;
+  }
 
-  set name(String name) => this._name = name;
-  String get name => this._name!;
-
-  set age(int age) => this._age = age;
-  int get age => this._age!;
-
-  set city(String city) => this._city = city;
-  String get city => this._city!;
-
-  set salary(int salary) => this._salary = salary;
-  int get salary => this._salary!;
-
-  void empInfo() {
-    print("$_name with id: $_id and has age $_age living in $_city with salary $_salary");
+  void display() {
+    print("Name: ${this.name}, Age: ${this.age}, City: ${this.city}, Salary: ${this.salary}");
   }
 }
 
@@ -34,42 +25,21 @@ void main() {
   List<String> city = ["Mumbai", "Indore", "Pune", "Hyderabad", "Surat", "Chennai", "Kanpur", "Raipur", "Udaipur", "Amritsar"];
   List<int> salary = [9000,12000,14000,25000,5000,8000,27000,3000,13000,33000];
 
-  List<Map<String, dynamic>> data = [];
+  Map<String, dynamic>data = {};
 
   for(int i = 0; i < 10; i++) {
-    Employee obj = Employee();
-    obj.id = i+1;
-    obj.name = names[Random().nextInt(names.length)];
-    obj.age = ages[Random().nextInt(ages.length)];
-    obj.city = city[Random().nextInt(city.length)];
-    obj.salary = salary[Random().nextInt(salary.length)];
+    Employee obj = Employee(names[i], ages[i], city[i], salary[i]);
 
-    Map<String, dynamic> mp = {
-      'id': obj.id,
-      'name': obj.name,
-      'age': obj.age,
-      'city': obj.city,
-      'salary': obj.salary
-    };
-
-    data.add(mp);
+    data.addAll({
+      "xyz${i+1}" : obj,
+    });
   }
 
   print("\n");
-  
-  print("Employees with age greater than 25: ");
-  data.forEach((employee) {
-    if(employee['age'] > 25) {
-      print(employee);
+  data.forEach((id, employeeObj) {
+    if(employeeObj.age > 25 && employeeObj.salary >= 10000 && employeeObj.salary <= 20000) {
+        employeeObj.display();
     }
   });
 
-  print("\n");
-
-  print("Employess with salary between 10K and 20K: ");
-  data.forEach((employee) {
-    if(employee['salary'] >= 10000 && employee['salary'] <= 20000) {
-      print(employee);
-    }
-  });
 }
